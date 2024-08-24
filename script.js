8a3914817b5bad65a5d1efc38e4e6b68f520aaf8
ezoooooooooo <ezooahmed39@gmail.com>
2024-08-24 15:38:36 +0300

    Make last function to play the game
    the last function takes the two choices from the
    other functions compare them and accroding to its
    comparing it generate winner or loser or a tie

--- script.js
+++ script.js
@@ -1,62 +1,96 @@
 /*
-*first function to get the computer choice
+ *first function to get the computer choice
  */
-function getComputerChoice(){
- /*
- * variable intiated to save the given value
-  */
- let x;
- /*
- * We have used Math.random function to get random number and multiply it by 3 so we keep it around 3
- * choices as we just need 3 and we used math.floor so we round the number down and return the int less
- * or equal to the given number so we keep it between 0,1 and 2 and we keep this value in a variable
- */
- x = Math.floor((Math.random() * 3));
-/*
-* now we make if statment so we make the function return one of the 3 choices based on the number generated
-* from the math.random method
-*/
-if(x==0){
- return "rock";
-}
-else if (x==1){
- return "paper";
-}
-else{
- return "scissors";
-}
-}
-/*
-* function to get human choice
-*/
-function getHumanChoice(){
- /*
-  * first we prompt a messeage to the user so he input his choice and we assign it to a variable
-  */
- y = prompt('Enter your choice');
- /*
- * we make sure its all lower case so we don't have a equality problem 
- */
- y = y.toLowerCase();
-/*
- * we make a loop with while the condition is true using if statements if y 
- * is equal to any of the correct choices it breaks the loop else the loop
- * continue with the message to the user to enter valid choice
-*/
- while (true){
-  if (y == "rock" || y == "paper" || y == "scissors"){
-   break;
+function getComputerChoice() {
+  /*
+   * variable intiated to save the given value
+   */
+  let x;
+  /*
+   * We have used Math.random function to get random number and multiply it by 3 so we keep it around 3
+   * choices as we just need 3 and we used math.floor so we round the number down and return the int less
+   * or equal to the given number so we keep it between 0,1 and 2 and we keep this value in a variable
+   */
+  x = Math.floor(Math.random() * 3);
+  /*
+   * now we make if statment so we make the function return one of the 3 choices based on the number generated
+   * from the math.random method
+   */
+  if (x == 0) {
+    return "rock";
+  } else if (x == 1) {
+    return "paper";
+  } else {
+    return "scissors";
   }
-  else{
-   y = prompt('Not Valid Choice, please enter a valid one (rock or paper or sicssors')
-  }
- }
- /*
-  * we return the y value so we can use it further in the game
+}
+/*
+ * function to get human choice
  */
- return y;
+function getHumanChoice() {
+  /*
+   * first we prompt a messeage to the user so he input his choice and we assign it to a variable
+   */
+  y = prompt("Enter your choice");
+  /*
+   * we make sure its all lower case so we don't have a equality problem
+   */
+  y = y.toLowerCase();
+  /*
+   * we make a loop with while the condition is true using if statements if y
+   * is equal to any of the correct choices it breaks the loop else the loop
+   * continue with the message to the user to enter valid choice
+   */
+  while (true) {
+    if (y == "rock" || y == "paper" || y == "scissors") {
+      break;
+    } else {
+      y = prompt(
+        "Not Valid Choice, please enter a valid one (rock or paper or sicssors"
+      );
+    }
+  }
+  /*
+   * we return the y value so we can use it further in the game
+   */
+  return y;
+}
+/*
+ * last function is play round to actually play the game
+ */
+function playRound(humanChoice, computerChoice) {
+  /*
+   * we made and if statement to compare between choices we used all human possible choices to win
+   * if his choice wasn't around those he will lose and computer wins and accroding to the winner
+   * we generate a messeage to the winner and increment winner score
+   * if it's a tie it prints it's a tie 
+   */
+  if (humanChoice == computerChoice) {
+    console.log("It's a tie");
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You Won, Rock beats Scissors");
+    humanScore++;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You Won, Paper beats Rock");
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You Won, Scissors beats Paper");
+    humanScore++;
+  } else {
+    console.log("You lost, " + computerChoice + " beats " + humanChoice);
+    computerScore++;
+  }
+}
 
-}
+/*
+ * two variables to hold scores
+ */
 let humanScore = 0;
 let computerScore = 0;
+/*
+ * variables to save human and computer choices and assign them to the function
+ */
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
 
+playRound(humanChoice, computerChoice);
